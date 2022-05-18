c5153af32aa52b852ed3d50094f76308748f2fd5
Michał <64927330+CenturionTheMan@users.noreply.github.com>
2022-05-18 09:53:40 +0200

    vector2 class update

--- main/Vector2.java
+++ main/Vector2.java
@@ -19,10 +19,32 @@ public class Vector2 {
         y += vec.y;
     }
 
+    public void MultiplyVector(int multi)
+    {
+        x *= multi;
+        y *= multi;
+    }
+
+    public void MultiplyVector(Vector2 multi)
+    {
+        x *= multi.x;
+        y *= multi.y;
+    }
+
+    public double GetLenght()
+    {
+        return Math.sqrt(x*x + y*y);
+    }
+
     public boolean Compare(Vector2 vec)
     {
         if(x == vec.x && y == vec.y) return true;
         else return false;
+    }
+
+    public static Vector2 MultiplyVecotrs(Vector2 first, Vector2 second)
+    {
+        return new Vector2(first.x * second.x, first.y * second.y);
     }
 
     public static Vector2 AddVectors(Vector2 first, Vector2 second)
