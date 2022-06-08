20de17fb83b2b80c4fba31c6f092ab2d0af8f967
Michał <64927330+CenturionTheMan@users.noreply.github.com>
2022-06-08 10:49:46 +0200

    unit test -> fixed

--- main/AppUnitTest.java
+++ main/AppUnitTest.java
@@ -1,6 +1,6 @@
 package main;
-/*
-import static org.junit.Assume.assumeTrue;
+
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -26,39 +26,39 @@ public class AppUnitTest {
 
         sim.InitSimulation();
 
-        assumeTrue(sim.GetPodbusInitAmount() == 6);
-        assumeTrue(sim.GetGimbusInitAmount() == 5);
-        assumeTrue(sim.GetPatusInitAmount() == 4);
-        assumeTrue(sim.GetLicbusInitAmount() == 3);
-        assumeTrue(sim.GetStudentInitAmount() == 2);
-        assumeTrue(sim.GetDebilInitAmount() == 1);
+        assertTrue(sim.GetPodbusInitAmount() == 6);
+        assertTrue(sim.GetGimbusInitAmount() == 5);
+        assertTrue(sim.GetPatusInitAmount() == 4);
+        assertTrue(sim.GetLicbusInitAmount() == 3);
+        assertTrue(sim.GetStudentInitAmount() == 2);
+        assertTrue(sim.GetDebilInitAmount() == 1);
 
+        //assertTrue(false);
 
         sim.RunSimulationWithoutNewThred();
         SimulationResult result = sim.GetResult();
 
         switch (result.numberOfEndCondition) {
             case 1:
-            assumeTrue(result.finNumberStudent > 0 && result.finNumberOfPiwo > 0);
+            assertTrue(result.finNumberStudent > 0 && result.finNumberOfPiwo > 0);
             break;
         
             case 2:
-            assumeTrue(result.finNumberOfPiwo == 0 && result.finNumberOfGimbus < 2 && result.finNumberOfPodbus == 0 && result.finNumberOfPatus == 0);
+            assertTrue(result.finNumberOfPiwo == 0 && result.finNumberOfGimbus < 2 && result.finNumberOfPodbus == 0 && result.finNumberOfPatus == 0);
             break;
 
             case 3:
-            assumeTrue(result.finNumberOfEgzamin ==0 && result.finNumberStudent ==0 && result.finNumberOfPodbus == 0 && result.finNumberOfLicbus == 0 && result.finNumberOfGimbus == 0 && result.finNumberOfDebil == 0);
+            assertTrue(result.finNumberOfEgzamin ==0 && result.finNumberStudent ==0 && result.finNumberOfPodbus == 0 && result.finNumberOfLicbus == 0 && result.finNumberOfGimbus == 0 && result.finNumberOfDebil == 0);
             break;
 
             case 4:
-            assumeTrue(result.finNumberOfPatus == 0&&result.finNumberStudent ==0 && result.finNumberOfPodbus == 0 && result.finNumberOfLicbus == 0 && result.finNumberOfGimbus == 0 && result.finNumberOfDebil == 0);
+            assertTrue(result.finNumberOfPatus == 0&&result.finNumberStudent ==0 && result.finNumberOfPodbus == 0 && result.finNumberOfLicbus == 0 && result.finNumberOfGimbus == 0 && result.finNumberOfDebil == 0);
             break;
 
             case 5:
-            assumeTrue(result.numberOfRounds == 600);
+            assertTrue(result.numberOfRounds == 600);
             break;
         }
 
     }
 }
-*/
